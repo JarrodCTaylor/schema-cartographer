092699c74ddb4e6988a3903d1eca4e184b36e352
JarrodCTaylor <jarrod@fastmail.com>
2023-01-12 20:38:54 -0600

    Redirect to split repos

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject schema-cartographer "1.1.0"
-  :description "Schema Cartographer provides a means to visualize, navigate, create, edit and share the relationships that exist in your Datomic schema."
-  :url "https://github.com/JarrodCTaylor/schema-cartographer"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/tools.cli "1.0.206"]
-                 [com.datomic/datomic-pro "1.0.6362"]
-                 [com.datomic/client-cloud "0.8.105"]])
